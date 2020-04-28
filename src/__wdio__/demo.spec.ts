2d2df2301c644592f23e34534bd2b98b3aa2ddc7
Diego Ferreiro Val <dval@salesforce.com>
2020-04-27 19:41:12 -0700

    feat: utam integration

--- src/__wdio__/demo.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('first test', () => {
-    it('PageObject is created correctly', async () => {
-        await browser.url('https://rawgit.com/ebidel/2d2bb0cdec3f2a16cf519dbaa791ce1b/raw/fancy-tabs-demo.html');
-        const element = await browser.findElement('css selector', 'fancy-tabs');
-        expect(element).toBeDefined();
-    });
-});
